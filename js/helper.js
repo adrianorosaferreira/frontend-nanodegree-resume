3810ffe7aa35631c285d7c32c39daa9795ae85b3
Adriano Ferreira <adrianorosaferreira@gmail.com>
2018-01-18 11:17:05 -0200

    feat: Commit inicial

---
+++ js/helper.js
@@ -0,0 +1,199 @@
+/*
+This file contains all of the code running in the background that makes resumeBuilder.js possible.
+*/
+
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span class="role">%data%</span><hr>';
+
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%" width="168px" height="76px">';
+
+var HTMLschoolStart = '<div class="education-entry"><hr></div>';
+var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+var HTMLonlineClasses = '<br><h2>Online Classes</h2>';
+var HTMLonlineStart = '<div class="education-online"><hr></div>';
+var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<br><a href="#">%data%</a>';
+
+var internationalizeButton = '<button>Internationalize</button>';
+var googleMap = '<div id="map"></div>';
+
+/*
+The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+*/
+function inName(name){
+  var interName = name.split(" ");
+  for (part in interName){
+    if (part > 0){
+      interName[part] = interName[part].toUpperCase();
+    }
+  }
+  interName = interName.join(" ");
+
+  return interName;
+}
+
+$(document).ready(function() {
+  $('button').click(function() {
+    var $name = $('#name');
+    var iName = inName($name.text()) || function(){};
+    $name.html(iName);
+  });
+});
+
+/*
+The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
+*/
+var clickLocations = [];
+
+function logClicks(x,y) {
+  clickLocations.push(
+    {
+      x: x,
+      y: y
+    }
+  );
+  console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function(loc) {
+  logClicks(loc.pageX, loc.pageY);
+});
+
+var map;  
+
+function initializeMap() {
+
+  var locations;
+
+  var mapOptions = {
+    disableDefaultUI: true
+  };
+
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+
+  /*
+  locationFinder() returns an array of every location string from the JSONs
+  written for bio, education, and work.
+  */
+  function locationFinder() {
+
+    var locations = [];
+
+    locations.push(bio.contacts.location);
+
+    education.schools.forEach(function(school){
+      locations.push(school.location);
+    });
+
+    work.jobs.forEach(function(job){
+      locations.push(job.location);
+    });
+
+    return locations;
+  }
+
+  /*
+  createMapMarker(placeData) reads Google Places search results to create map pins.
+  placeData is the object returned from search results containing information
+  about a single location.
+  */
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var name = placeData.formatted_address;   // name of the place from the place service
+    var bounds = window.mapBounds;            // current boundaries of the map window
+
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    map.fitBounds(bounds);
+    map.setCenter(bounds.getCenter());
+  }
+
+  /*
+  callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location.
+  */
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0]);
+    }
+  }
+
+  /*
+  pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location
+  */
+  function pinPoster(locations) {
+
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterates through the array of locations, creates a search object for each location
+      locations.forEach(function(place){
+      // the search request object
+      var request = {
+        query: place
+      };
+
+      service.textSearch(request, callback);
+    });
+  }
+
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  locations = locationFinder();
+
+  // pinPoster(locations) creates pins on the map for each location in
+  // the locations array
+  pinPoster(locations);
+
+}
+
+window.addEventListener('load', initializeMap);
+
+window.addEventListener('resize', function(e) {
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,252 @@
+var data = "%data%";
+
+var bio = {
+		"name" : "Adriano Ferreira", 
+		"role" : "Developer and System Analyst", 
+		"welcomeMessage" : "\"Doubt is the principle of wisdom\"", 
+		"contacts" : {
+			"mobile" : "+55 (48) 98827-7554", 
+			"email" : "adrianorosaferreira@gmail.com", 
+			"github" : "adrianorosaferreira", 
+			"linkedin" : "adrianorosaferreira",
+			"twitter" : "@4drianoFerreira", 
+			"location" : "Florianópolis, SC, Brazil"
+		},
+		"skills" : [ 
+			"JavaScript", "Quality Assurance", "Java", "AngularJS"
+		],
+		"pic" : "images/AdrianoFerreira_95x72.png",
+		displayHeader : function(){
+
+			var formattedName = HTMLheaderName.replace( data, bio.name);
+			$("#topHeader").append(formattedName);
+			var formattedRole = HTMLheaderRole.replace( data, bio.role);
+			$("#topHeader").append(formattedRole);
+
+			var formattedLocation = HTMLlocation.replace( data,bio.contacts.location);
+			$("#topContacts").append(formattedLocation);
+			var formattedMobile = HTMLmobile.replace( data,bio.contacts.mobile);
+			$("#topContacts").append(formattedMobile);
+			var formattedContact = HTMLcontactGeneric.replace( data,bio.contacts.linkedin);
+			formattedContact = formattedContact.replace("%contact%","linkedin");
+			$("#topContacts").append(formattedContact);
+			var formattedEmail = HTMLemail.replace( data,bio.contacts.email);
+			$("#topContacts").append(formattedEmail);
+			var formattedTwitter = HTMLtwitter.replace( data,bio.contacts.twitter);
+			$("#topContacts").append(formattedTwitter);
+			var formattedGitHub = HTMLgithub.replace( data,bio.contacts.github);
+			$("#topContacts").append(formattedGitHub);
+
+			var formattedPic = HTMLbioPic.replace( data, bio.pic);
+			$("#skills-entry").append(formattedPic);
+			var formattedWelcomeMsg = HTMLwelcomeMsg.replace( data, bio.welcomeMessage);
+			$("#skills-entry").append(formattedWelcomeMsg);
+
+			if (bio.skills.length > 0){
+				$("#skills-entry").append(HTMLskillsStart);
+
+				bio.skills.forEach(function(skill) {
+				    var formattedSkill = HTMLskills.replace( data, skill);
+					$("#skills").append(formattedSkill);
+				});
+			}
+		},
+		displayFooter : function(){
+
+			var formattedLocation = HTMLlocation.replace( data,bio.contacts.location);
+			$("#footerContacts").append(formattedLocation);
+			var formattedMobile = HTMLmobile.replace( data,bio.contacts.mobile);
+			$("#footerContacts").append(formattedMobile);
+			var formattedContact = HTMLcontactGeneric.replace( data,bio.contacts.linkedin);
+			formattedContact = formattedContact.replace("%contact%","linkedin");
+			$("#footerContacts").append(formattedContact);
+			var formattedEmail = HTMLemail.replace( data,bio.contacts.email);
+			$("#footerContacts").append(formattedEmail);
+			var formattedTwitter = HTMLtwitter.replace( data,bio.contacts.twitter);
+			$("#footerContacts").append(formattedTwitter);
+			var formattedGitHub = HTMLgithub.replace( data,bio.contacts.github);
+			$("#footerContacts").append(formattedGitHub);
+
+		}
+}
+
+var work = {
+	"jobs": [
+		{
+			"employer" : "Ibrowse", 
+			"title" : "System Analyst/ Fullstack Developer",
+			"location" : "São José, SC, Brazil",
+			"dates" : "Sept. 2016 – Currently",
+			"description" : "Independently develop systems to support other analysts and/or developers maintenance of legacy systems of the Santa Catarina Court of Justice. Propose new solutions or functions based on customer requirements received and draw up plans in order tests to ensure the quality of routines and processes under my responsibility. Perform requirements gathering and lead the team to solution development. Create, monitor and execute the test plan, individually or in teams.Technologies: Angular JS, Bootstrap, Java, Caché."
+		},{
+			"employer" : "ArcTouch", 
+			"title" : "Quality Assurance Specialist",
+			"location" : "Florianópolis, SC, Brazil",
+			"dates" : "Jan 2016 – jun. 2016",
+			"description" : "My main functions are: to plan, conduct and monitor testing of products to ensure quality. Document internal audits and other quality assurance activities. Collect and compile statistical quality. Develop, recommend and monitor corrective and preventive actions. Develop sampling procedures and instructions for recording and reporting of quality data. Investigate customer complaints and non-compliance issues, among others"
+		},{
+			"employer" : "Conceitual", 
+			"title" : "System Analyst/ Tester",
+			"location" : "São José, SC, Brazil",
+			"dates" : "Oct. 2014 – Dec. 2015",
+			"description" : "Independently develop systems to support other analysts and / or developers for maintenance of legacy systems of the Santa Catarina Court of Justice. Propose new solutions or functions based on customer requirements received and draw up plans in order tests to ensure the quality of routines and processes under my responsibility. Perform requirements gathering and lead the team to solution development. Create, monitor and execute the test plan, individually or in teams. "
+		}
+	],
+	display : function(){
+		if (work.jobs.length > 0){
+			work.jobs.forEach(function(job) {
+			    $("#workExperience").append(HTMLworkStart);
+
+				var formattedEmployer = HTMLworkEmployer.replace( data, job.employer);
+				var formattedTitle = HTMLworkTitle.replace( data, job.title);
+				var formatterEmployerTitle = formattedEmployer + formattedTitle;
+				$(".work-entry:last").append(formatterEmployerTitle);
+				var formattedDates = HTMLworkDates.replace( data, job.dates);
+				$(".work-entry:last").append(formattedDates);
+				var formattedLocation = HTMLworkLocation.replace( data, job.location);
+				$(".work-entry:last").append(formattedLocation);
+				var formattedDescription = HTMLworkDescription.replace( data, job.description);
+				$(".work-entry:last").append(formattedDescription);
+
+			});
+		}
+	}
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "Curriculo Online", 
+			"dates" : "2017", 
+			"description" : "Este projeto consiste na criação de um curriculo online onde, com o auxiliao de um script JS, combina minhas informações pessoais com os templates HTML e CSS pré-elaborados para gerar o meu currículo. O mesmo foi criado com uma sintaxe básica do JavaScript, que inclui manipulação de dados e criação de loops e funções. Ao mesmo tempo, ilustra a transformação de uma página Web estática em uma aplicação dinâmica através da utilização de alguns métodos simples de manipulação de jQuery DOM para criação do currículo no momento que um usuário abrir seu site.",
+			"images" : [
+				"images/resumeOnline_1.png",
+				"images/resumeOnline_2.png"
+			]
+		},{
+			"title" : "Site de Portifólio", 
+			"dates" : "2017", 
+			"description" : "Este projeto apresenta meu site de portfólio. Ele foi criado a partir de um mockup fornecido pela Udacity, onde tive que manter o design criado com HTML e CSS e aplicar as guias de estilos fornecidas pela mesma. Este projeto consiste em um site responsivo que exibe imagens, descrições e links para cada projeto que completei no Nanodegree Front-End.",
+			"images" : [
+				"images/portifolio_mockup.png",
+				"images/portifolio_bootstrap.png",
+				"images/portifolio_framework.png"
+			]
+		}
+	],
+	"display" : function() {
+
+		projects.projects.forEach(function(project) {
+		    $("#projects").append(HTMLprojectStart); 
+
+			var formattedTitle = HTMLprojectTitle.replace( data, project.title);
+			$(".project-entry:last").append(formattedTitle); 
+			var formattedDates = HTMLprojectDates.replace( data, project.dates);
+			$(".project-entry:last").append(formattedDates); 
+			var formattedDescription = HTMLprojectDescription.replace( data, project.description);
+			$(".project-entry:last").append(formattedDescription); 
+
+			project.images.forEach(function(image) {
+				var formattedImage = HTMLprojectImage.replace( data, image);
+				$(".project-entry:last").append(formattedImage);
+			});	
+		});
+	}
+}
+	
+var education = { 
+	"schools": [
+		{
+			"name" : "MBA in Project Management (Fundação Getulio Vargas)", 
+			"location" : "Florianópolis, Santa Catarina", 
+			"degree" : "Graduate", 
+			"majors"  : [ "Postgraduate certificate" ], 
+			"dates" : "2013",
+			"url" : "http://www.sociesc.org.br/pt/fgv/conteudo.php?id=429&mnu=3785&top=0&crs=416"
+		},{
+			"name" : "Information Systems (Unisul)", 
+			"location" : "Palhoça, Santa Catarina", 
+			"degree" : "Undergraduate degree", 
+			"majors"  : [ "Bachelor's degree" ], 
+			"dates" : "2010",
+			"url" : "http://www.unisul.br/wps/portal/home/ensino/graduacao/sistemas-de-informacao/"
+		},{
+			"name" : "Telecommunications Technician (Escola Técnica Federal)", 
+			"location" : "São José, Santa Catarina", 
+			"degree" : "High school + Associate degree", 
+			"majors"  : [ "Associate degree" ], 
+			"dates" : "1998",
+			"url" : "https://curso.ifsc.edu.br/info/tecnicointegrado/tecnicointegradotelecomunicacoes/SJE"
+		},{ 
+			"name" : "English Course - ESL for Adults (SSLC Vancouver)", 
+			"location" : "Vancouver, BC, Canada", 
+			"degree" : "",
+			"majors"  : [ "Not Applicable" ], 
+			"dates" : "2015",
+			"url" : "http://www.studysslc.com/esl/"
+		}
+	],
+	"onlineCourses" : [
+		{ 
+			"title" : "Nanodegree Development Web Front-End", 
+			"school" : "Udacity", 
+			"dates" : "2017 - Currently",
+			"url" : "https://br.udacity.com/course/front-end-web-developer-nanodegree--nd001/"
+		},{ 
+			"title" : "Full-Stack Web Development", 
+			"school" : "Courcera", 
+			"dates" : "2016 - Currently",
+			"url" : "https://www.coursera.org/specializations/full-stack-mobile-app-development"
+		},{ 
+			"title" : "HTML, CSS e JavaScript", 
+			"school" : "Courcera", 
+			"dates" : "2016",
+			"url" : "https://www.coursera.org/learn/html-css-javascript"
+		}
+	],
+	display : function(){
+		
+		$("#education").append(HTMLschoolStart);
+		
+		education.schools.forEach(function(school) {
+			var formattedEduName = HTMLschoolName.replace( data, school.name);
+			var formattedEduUrl = formattedEduName.replace("#", school.url);
+			var formattedEduDegree = HTMLschoolDegree.replace( data, school.degree);
+			$(".education-entry:last").append(formattedEduUrl + formattedEduDegree);
+			var formattedEduDates = HTMLschoolDates.replace( data, school.dates);
+			$(".education-entry:last").append(formattedEduDates);
+			var formattedEduLocation = HTMLschoolLocation.replace( data, school.location);
+			$(".education-entry:last").append(formattedEduLocation);
+			var formattedEduMajor = HTMLschoolMajor.replace( data, school.majors);
+			$(".education-entry:last").append(formattedEduMajor);
+		});
+
+		$("#education:last").append(HTMLonlineClasses);
+		$("#education:last").append(HTMLonlineStart);
+		
+		education.onlineCourses.forEach(function(course) {
+			var formattedOLTitle = HTMLonlineTitle.replace( data, course.title);
+			var formattedOLTitle = formattedOLTitle.replace("#", course.url);
+			var formattedOLSchool = HTMLonlineSchool.replace( data, course.school);
+			$(".education-online").append(formattedOLTitle + formattedOLSchool);
+			var formattedOLDates = HTMLonlineDates.replace( data, course.dates);
+			$(".education-online").append(formattedOLDates);
+			var formattedOLURL = HTMLonlineURL.replace( data,"Site");
+			var formattedOLURL = formattedOLURL.replace("#", course.url);
+			$(".education-online").append(formattedOLURL);
+		});
+	}	
+}
+
+bio.displayHeader();
+work.display();
+education.display();
+projects.display();
+bio.displayFooter();
+
+$("#mapDiv").append(googleMap);
+
+	
+	
+
+
